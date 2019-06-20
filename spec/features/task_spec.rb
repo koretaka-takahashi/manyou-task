de3059585764151e5d21f05240d4a12c72dcb399
高橋是高 <shcrad.kore@gmail.com>
2019-06-20 19:14:03 +0900

    タスク一覧を作成日時で並び替え,

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,46 +1,35 @@
 require 'rails_helper'
 
 RSpec.feature "タスク管理機能", type: :feature do
-
-  background do
-    # Task.create!(name: 'test1', content: 'test1をやる')
-    # Task.create!(name: 'test2', content: 'test2をやる')
-    FactoryBot.create(:task)
-    FactoryBot.create(:task2)
-  end
-
-  scenario "タスク一覧のテスト" do
-    # tasks_pathにvisitする（タスク一覧ページに遷移する）
-    visit tasks_path
-
-    # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
-    # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
-    expect(page).to have_content 'test1をやる'
-    expect(page).to have_content 'test2をやる'
-  end
-
-  scenario "タスク作成のテスト" do
+  scenario "タスクが作成される" do
     visit new_task_path
     fill_in 'タスク', with: 'test1'
     fill_in '内容', with: 'test1をやる'
     click_on '登録する'
     expect(page).to have_content 'test1をやる'
-
-    # expect {
-    #   visit new_task_path
-    #   fill_in 'タスク', with: 'test1'
-    #   fill_in '内容', with: 'test1をやる'
-    #   click_on '登録する'
-    # }.to change(Task, :count).by(1)
   end
 
-  scenario "タスク詳細のテスト" do
+  scenario "タスクの詳細が表示される" do
     test_task = Task.create!(name: 'test1', content: 'test1をやる')
     visit task_path(test_task.id)
     expect(page).to have_content "test1をやる"
   end
 
-  scenario "タスク一覧の作成日時による並び替えテスト" do
-    
+  feature "タスク一覧画面のテスト" do
+    background do
+      FactoryBot.create(:task, id: 1, updated_at: Time.current + 1.days)
+      FactoryBot.create(:task2, id: 2, updated_at: Time.current + 2.days)
+    end
+
+    scenario "タスク一覧が表示される" do
+      visit tasks_path
+      expect(page).to have_content 'test1をやる'
+      expect(page).to have_content 'test2をやる'
+    end
+
+    scenario "タスク一覧が作成日時による並び替えられる" do
+      visit tasks_path
+      expect(Task.order(created_at: "DESC").map(&:id)).to eq [2, 1]
+    end
   end
 end
