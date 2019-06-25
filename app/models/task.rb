c4746faf2432acc21fe9c5ade8bc43e027cb76fe
高橋是高 <shcrad.kore@gmail.com>
2019-06-25 13:47:05 +0900

    バリデーションおよびテスト作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :name, presence: true, length: {maximum: 30}
-  validates :content, presence: true, length: {maximum: 1000}
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { maximum: 1000 }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,17 +3,33 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
 
   it "nameが空だとバリデーションエラーになること" do
-    task = Task.new(name: '', content: 'test1をやる')
-    expect(task).to be_valid
+    # task = Task.new(name: '', content: 'test1をやる')
+    task = FactoryBot.build(:task, name: '')
+    expect(task).not_to be_valid
   end  
 
   it "contentが空だとバリデーションエラーになること" do
-  end  
+    task = FactoryBot.build(:task, content: '')
+    expect(task).not_to be_valid
+  end
 
   it "name,contentに値が入っていると保存されること" do
+    task = FactoryBot.build(:task, name: 'a', content: 'aa')
+    expect(task).to be_valid
   end
 
-  it "contentにデフォルトの値が入っていること" do
+  it "contentにデフォルト値が入ること" do
+    task = Task.new(name: 'a')
+    expect(task.content).to eq '作成中…'
   end
 
+  it "nameが31文字以上でエラーになること" do
+    task = FactoryBot.build(:task, name: 'a' * 31)
+    expect(task).not_to be_valid
+  end
+
+  it "contentが1001文字以上でエラーになること" do
+    task = FactoryBot.build(:task, content: 'a' * 1001)
+    expect(task).not_to be_valid
+  end
 end
