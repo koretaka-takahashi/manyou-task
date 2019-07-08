abe7c883a4f8b6e6370f9f0f881fc8f9123f4d8d
高橋是高 <shcrad.kore@gmail.com>
2019-07-08 20:15:41 +0900

    deadlineカラムにnull: falseを付けるため奮闘中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_085340) do
+ActiveRecord::Schema.define(version: 2019_07_08_071144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_06_21_085340) do
     t.text "content", default: "作成中…", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "deadline"
   end
 
 end
