97ad80197458b980a7dee54a81f07c5f86cc80fc
高橋是高 <shcrad.kore@gmail.com>
2019-09-13 17:21:47 +0900

    Add:ラベル及びタスクとラベルの中間テーブル作成、アソシエーション定義

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :task_labels, dependent: :destroy
+  has_many :labels, through: :task_labels
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 1000 }
