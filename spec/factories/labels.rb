0d5027a25cbcd5eb006bb2c9886a21bb716ba694
高橋是高 <shcrad.kore@gmail.com>
2019-09-25 18:02:13 +0900

    Update: ラベル機能のFeatureSpec追記

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :label do
-    name { "MyString" }
+    name { "A" }
+  end
+
+  factory :label2, class: Label do
+    name { "B" }
+  end
+
+  factory :label3, class: Label do
+    name { "C" }
   end
 end
